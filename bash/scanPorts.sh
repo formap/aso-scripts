84e29df6855073b392cbf5ee90f099ac76c72147
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2016-05-31 23:08:26 +0200

    script to scan ports with nmap

---
+++ bash/scanPorts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# first parameter: IP range; second parameter: PORT(S)
+usage="Usage: scanPorts.sh [IP range] [ports]"
+
+if [ $# -ne 2 ]; then
+  echo $usage;
+  exit 1
+fi
+
+echo `nmap $1 -p $2 --open | awk '/is up/ {print up}; {gsub (/\(|\)/,""); up = $NF}'`;
