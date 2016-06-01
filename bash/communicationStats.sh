67805960894a30baebe0fa317825ed41f50ee9e8
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2016-06-01 09:39:24 +0200

    improved communication script

--- bash/communicationStats.sh
+++ bash/communicationStats.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+  echo "Wrong parameters"
+  exit 1
+fi
 
-echo `netstat -i | tail -n +3 | awk '{print $1 ":    " $8}'`
+while true
+do
+  echo `netstat -i | tail -n +3 | awk '{print $1 ":    " $8}'`
+  `sleep $1`
+done
